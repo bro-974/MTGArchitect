21f7fce37e8e2001cc83b28ef9732eb1c686c339
bro-974 <bruno.roger.974@gmail.com>
2026-05-06 09:03:58 +0200

    initialisation projet AI

---
+++ back/MTGArchitect.AI.Client/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace MTGArchitect.AI.Client
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ back/MTGArchitect.AI.Client/Module.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MTGArchitect.Scryfall.Client;
+
+public static class Module
+{
+    public static IServiceCollection AddScryfallClient(this IServiceCollection services, Uri scryfallService)
+    {
+        //services.AddGrpcClient<>(options =>
+        //{
+        //    options.Address = scryfallService;
+        //});
+        //services.AddScoped<>();
+        return services;
+    }
+}
+
